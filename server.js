eb515c843cc90e07b909a265825c877134e73859
Sonarie <sonarie1980@gmail.com>
2022-03-18 19:09:20 -0500

    getting caught up

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const express = require('express');
+const express = require("express");
+const db = require("./db/connection");
+const apiRoutes = require("./routes/apiRoutes");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
+// Use apiRoutes
+app.use("/api", apiRoutes);
 
-app.listen(PORT, () => {
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Start server after DB connection
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected.");
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+});
